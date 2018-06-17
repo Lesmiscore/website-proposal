e32b558ebe4fb9d7bdc4345a9583dbb853fbfbc3
nao20010128nao <nao20010128@gmail.com>
2018-06-18 00:40:31 +0900

    ランダム文字以外OK
    最低限は満たした

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ console.log("Loaded");
 const $ = require("jquery");
 
 $(() => {
+    // links
     $(".links-a-list").hover(function () {
         $("#links-link-preview").text($(this).attr("href"));
     }, () => {});
+    // images
+    const images = ["rin.png", "miku.png"];
+    let imageOffset = 0;
+    setInterval(() => {
+        imageOffset = (imageOffset + 1) % images.length;
+        $(".images-display").css("background-image", `url(${images[imageOffset]})`);
+    }, 5000);
 });
