78dceb5225c0a622cd3af02838993776fb33e54c
nao20010128nao <nao20010128@gmail.com>
2018-06-20 20:12:35 +0900

    png以外一個にまとめる

---
+++ utils/inliner.js
@@ -0,0 +1,33 @@
+const cheerio = require("cheerio");
+const fs = require("fs");
+const path = require("path");
+
+const staticPath = path.join(process.cwd(), "static");
+const staticResult = path.join(process.cwd(), "public");
+
+const indexHtml = fs.readFileSync(path.join(staticPath, "index.html"), 'UTF-8');
+
+const $ = cheerio.load(indexHtml);
+
+$('script').each(function (i, elem) {
+    elem = $(this);
+    const src = elem.attr("src");
+    console.log(`Inlining ${src}`);
+    const code = fs.readFileSync(path.join(staticPath, src), 'UTF-8');
+    elem.text(code);
+    elem.removeAttr(src);
+});
+
+let css = "";
+$('link[rel=\"stylesheet\"]').each(function (i, elem) {
+    elem = $(this);
+    const src = elem.attr("href");
+    console.log(`Inlining ${src}`);
+    const code = fs.readFileSync(path.join(staticPath, src), 'UTF-8');
+    css += `${code}\n`;
+    elem.remove();
+});
+
+$('head').append(`<style>\n${css}</style>`);
+
+fs.writeFileSync(path.join(staticResult, "index.html"), $.html());
